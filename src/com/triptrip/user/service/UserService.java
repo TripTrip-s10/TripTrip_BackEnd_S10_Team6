98c8da97fde23e108fed226459eb6f5bf8e0ed9b
jsm6616 <jsm6616@naver.com>
2023-03-30 16:06:49 +0900

    :rocket: css

--- src/com/triptrip/user/service/UserService.java
+++ src/com/triptrip/user/service/UserService.java
@@ -7,8 +7,6 @@ import com.triptrip.user.dto.User;
 public interface UserService {
 	void join(User user) throws SQLException;
 	User login(String auth_id, String auth_pw) throws SQLException;
-//	void modifyInfo(User user)throws SQLException;
 	void withdraw(int user_id)throws SQLException;
 	void modifyInfo(int userPk, String userName, String userMsg) throws SQLException;
 	User findById(int userPk) throws SQLException;
-}
